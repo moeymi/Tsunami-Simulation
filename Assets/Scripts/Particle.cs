d3b4b882d7c2e6265e464f369fa41f5dff35c057
humam.husam.eldeen@gmail.com <humam.husam.eldeen@gmail.com>
2022-05-21 19:24:27 +0300

    Adeed SPH Manager (Under Development)

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle 
+{
+    public Vector3 Position = Vector3.zero;
+    public Vector4 Color; 
+}
